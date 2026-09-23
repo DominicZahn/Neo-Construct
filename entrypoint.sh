6e12c03ff3e4ecad39166cb11493c7c3a636e939
DominicZahn <zahndominic@web.de>
2026-09-23 16:48:09 +0200

    enabled additions of to venv by user

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,17 +10,17 @@ fi
 
 CONFLICT_USER=$(getent passwd "${HOST_UID}" | cut -d: -f1)
 if [ -n "${CONFLICT_USER}" ] && [ "${CONFLICT_USER}" != "${USERNAME}" ]; then
-    usermod -u 9999 "${CONFLICT_USER}" || true
-    groupmod -g 9999 "${CONFLICT_USER}" || true
+  usermod -u 9999 "${CONFLICT_USER}" || true
+  groupmod -g 9999 "${CONFLICT_USER}" || true
 fi
 
 if ! id "${USERNAME}" >/dev/null 2>&1; then
-    groupadd -g "${HOST_GID}" "${USERNAME}" || true
-    useradd -u "${HOST_UID}" -g "${HOST_GID}" -s /bin/bash -m  -d "/home/${USERNAME}/" "${USERNAME}"
+  groupadd -g "${HOST_GID}" "${USERNAME}" || true
+  useradd -u "${HOST_UID}" -g "${HOST_GID}" -s /bin/bash -m -d "/home/${USERNAME}/" "${USERNAME}"
 else
-    usermod -u "${HOST_UID}" -g "${HOST_GID}" "${USERNAME}"
+  usermod -u "${HOST_UID}" -g "${HOST_GID}" "${USERNAME}"
 fi
 
-chown -R "${USERNAME}:${USERNAME}" "/home/${USERNAME}"
-echo "${USERNAME} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+chown -R "${USERNAME}:${USERNAME}" "/home/${USERNAME}" /opt/venv
+echo "${USERNAME} ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
 exec gosu "${USERNAME}" "$@"
